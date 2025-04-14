366d94820a3979a5a20ac2acd05fdc9094673941
unknown <lktra30@gmail.com>
2025-04-13 21:07:10 -0300

    Corrige o erro de importação do toast, substituindo react-hot-toast por sonner

--- src/app/components/EntranceForm.tsx
+++ src/app/components/EntranceForm.tsx
@@ -1,6 +1,6 @@
 import React, { FormEvent } from 'react';
 import { useRouter } from 'next/router';
-import { toast } from 'react-hot-toast';
+import { toast } from "sonner";
 import { getVisitorByCPF, updateVisitor, createVisitor, createAccessLog } from '../services/api';
 import { AccessLog, Visitor } from '../types';
 
